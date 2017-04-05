8b6c369290343013030a95fa1da6468fc6f2683f
מורן עמר <lizmoran.amar@gmail.com>
2017-04-05 23:27:32 +0300

    starting adapter

---
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/SearcherPriority.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Priority_Queue;
+namespace SearchAlgorithmsLib
+{
+    public abstract class SearcherPriority<T> : ISearcher<T>
+    {
+        private SimplePriorityQueue<State<T>> openList;
+        private int evaluatedNodes;
+
+        public int OpenListSize
+        {
+            get { return openList.Count; }
+        }
+
+        public SearcherPriority()
+        {
+            openList = new SimplePriorityQueue<State<T>>();
+            evaluatedNodes = 0;
+        }
+        protected State<T> popOpenList()
+        {
+            evaluatedNodes++;
+            return openList.Dequeue();
+        }
+        protected void addToOpenList(State<T> state)
+        {
+            this.openList.Enqueue(state, (float)state.cost);
+        }
+        protected bool openContaines(State<T> stateToFind)
+        {
+            foreach(State<T> s in this.openList)
+            {
+                if (s.Equals(stateToFind))
+                    return true;
+            }
+                return false;
+        }
+        
+        public List<State<T>> createBackTrace(State<T> goalState)
+        {
+            Stack<State<T>> backTraceStack = new Stack<State<T>>();
+            List<State<T>> backTrace = new List<State<T>>();
+            State<T> current = goalState;
+            while (current != null)
+            {
+                backTraceStack.Push(current);
+                current = current.cameFrom;
+            }
+            while (backTraceStack.Count() != 0)
+            {
+                backTrace.Add(backTraceStack.Pop());
+            }
+            return backTrace;
+        }
+
+        public void updateEvaluatedCount()
+        {
+            this.evaluatedNodes++;
+        }
+        public abstract Solution<T> search(ISearchable<T> searchable);
+        public int getNumberOfNodesEvaluated()
+        {
+            return evaluatedNodes;
+        }
+    }
+}
