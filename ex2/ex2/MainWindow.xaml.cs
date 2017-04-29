2051666a47dfdbc5342139994e18f9acdf522778
גילי מרדכייב <gilimordehayev@gmail.com>
2017-04-29 17:17:33 +0300

    view, no logic

---
+++ ex2/ex2/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ex2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void clickSinglePlayer(object sender, RoutedEventArgs e)
+        {
+            Window singleManu = new singlePlayerManu();
+            this.Hide();
+            singleManu.Show();
+        }
+
+        private void clickMultiPlayer(object sender, RoutedEventArgs e)
+        {
+            Window multiManu = new MultiPlayerManu();
+            this.Hide();
+            multiManu.Show();
+        }
+
+        private void settings_click(object sender, RoutedEventArgs e)
+        {
+            Window settings = new SettingManu();
+            this.Hide();
+            settings.Show();
+        }
+    }
+}
---
+++ ex2/ex2/singlePlayerManu.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ex2
+{
+    /// <summary>
+    /// Interaction logic for singlePlayerManu.xaml
+    /// </summary>
+    public partial class singlePlayerManu : Window
+    {
+        public singlePlayerManu()
+        {
+            InitializeComponent();
+        }
+
+        private void startGame_Click(object sender, RoutedEventArgs e)
+        {
+            int row, col;
+            if (mazeNameValue.Text == "" || mazeNameValue.Text == "enter name here"
+                || rowNum.Text == "" || rowNum.Text == "enter rows here" 
+                ||colNum.Text == "" || colNum.Text == "enter columns here")
+            {
+                MessageBox.Show("some fileds are missing");
+            } else
+            {
+                //double.TryParse(candidate, out num)
+                if (!int.TryParse(rowNum.Text, out row) || !int.TryParse(colNum.Text, out col))
+                {
+                    MessageBox.Show("rows & cols must be an integer");
+                }
+            }
+        }
+
+        private void textRemoveName(object sender, RoutedEventArgs e)
+        {
+            if (mazeNameValue.Text == "enter name here")
+            {
+
+                mazeNameValue.Text = "";
+            }
+        }
+        private void textRemoveRow(object sender, RoutedEventArgs e)
+        {
+            if (rowNum.Text == "enter rows here")
+            {
+                rowNum.Text = "";
+            }
+        }
+        private void textRemoveCol(object sender, RoutedEventArgs e)
+        {
+            if (colNum.Text == "enter columns here")
+            {
+                colNum.Text = "";
+            }
+        }
+
+        private void defTextName(object sender, RoutedEventArgs e)
+        {
+            if (mazeNameValue.Text == "")
+            {
+
+                mazeNameValue.Text = "enter name here";
+            }
+        }
+        private void defTextRow(object sender, RoutedEventArgs e)
+        {
+            if (rowNum.Text == "")
+            {
+                rowNum.Text = "enter rows here";
+            }
+        }
+        private void defTextCol(object sender, RoutedEventArgs e)
+        {
+            if (colNum.Text == "")
+            {
+                colNum.Text = "enter columns here";
+            }
+        }
+    }
+}
